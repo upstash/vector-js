a21bfb0ac5406960220d86f7b6806224c61785c8
ytkimirti <yusuftaha9@gmail.com>
2024-09-24 10:23:07 +0300

    chore: unify exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["./src/platforms/nodejs.ts", "./src/platforms/cloudflare.ts"],
   format: ["cjs", "esm"],
-  sourcemap: false,
   clean: true,
   dts: true,
-  minify: true,
 });
