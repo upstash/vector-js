281c4b9009191e4d14ba9a944cdb7ca08f76635b
CahidArda <cahidardaooz@hotmail.com>
2024-10-04 12:00:13 +0300

    feat: add cf and nextjs examples

---
+++ examples/cloudflare-workers/src/index.ts
@@ -0,0 +1,26 @@
+import { Index } from "@upstash/vector/cloudflare";
+
+const id = "my-id"
+
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+
+    const index = new Index({
+      url: env.UPSTASH_VECTOR_REST_URL,
+      token: env.UPSTASH_VECTOR_REST_TOKEN,
+    })
+
+    await new Promise(r => setTimeout(r, 1000));
+
+    const result = await index.fetch([id], { includeData: true })
+
+    console.log(`length: ${result.length}`);
+    const data = result[0]?.data
+    console.log(`data: ${data}`);
+
+    return new Response(
+      JSON.stringify({ data, id: result[0]?.id }),
+      { status: 200 }
+    )
+	},
+} satisfies ExportedHandler<Env>;
---
+++ examples/nextjs/src/app/api/route.ts
@@ -0,0 +1,29 @@
+import { Index } from "@upstash/vector";
+import { NextResponse } from "next/server";
+
+const index = new Index({
+  url: process.env.UPSTASH_VECTOR_REST_URL,
+  token: process.env.UPSTASH_VECTOR_REST_TOKEN,
+})
+
+const id = "my-id"
+
+export const GET = async () => { 
+  await index.upsert({
+    id,
+    data: "hello world"
+  })
+
+  await new Promise(r => setTimeout(r, 1000));
+
+  const result = await index.fetch([id], { includeData: true })
+
+  console.log(`length: ${result.length}`);
+  const data = result[0]?.data
+  console.log(`data: ${data}`);
+  
+  return new NextResponse(
+    JSON.stringify({ data, id: result[0]?.id }),
+    { status: 200 }
+  )
+}
