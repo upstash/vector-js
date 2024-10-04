449372808be2a5a24d6f09801c5e580ed591ca24
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-10-04 15:48:10 +0300

    fix

--- examples/cloudflare-workers/ci.test.ts
+++ examples/cloudflare-workers/ci.test.ts
@@ -1,12 +1,13 @@
 import { test, expect, } from "bun:test";
 
-const deploymentURL = process.env.DEPLOYMENT_URL;
+const deploymentURL = process.env.DEPLOYMENT_URL ?? "http://127.0.0.1:8787";
 if (!deploymentURL) {
 	throw new Error("DEPLOYMENT_URL not set");
 }
 
 test("the server is running", async () => {
 	const res = await fetch(deploymentURL);
+	console.log(await res.text());
 	if (res.status !== 200) {
 		console.log(await res.text());
 	}
