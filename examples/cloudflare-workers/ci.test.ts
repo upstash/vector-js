60e0972827c5da26953a55c810f26eba1b2ec699
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-10-04 15:38:24 +0300

    add test

--- examples/cloudflare-workers/ci.test.ts
+++ examples/cloudflare-workers/ci.test.ts
@@ -1,7 +1,16 @@
-import { test, assert, describe } from "vitest";
+import { test, expect, } from "bun:test";
 
-describe('TEST', () => {
-	test("Hello World!", async () => {
-		assert.equal("Hello World!", "Hello World!");
-	})
-})
+const deploymentURL = process.env.DEPLOYMENT_URL;
+if (!deploymentURL) {
+	throw new Error("DEPLOYMENT_URL not set");
+}
+
+test("the server is running", async () => {
+	const res = await fetch(deploymentURL);
+	if (res.status !== 200) {
+		console.log(await res.text());
+	}
+	expect(res.status).toEqual(200);
+});
+
+
