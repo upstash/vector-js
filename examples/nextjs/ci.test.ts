6004cbb454f1b6315be8a2f423ea6f71f41570db
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-10-05 18:02:43 +0300

    fix test

--- examples/nextjs/ci.test.ts
+++ examples/nextjs/ci.test.ts
@@ -1,6 +1,6 @@
 import { test, expect, } from "bun:test";
 
-const deploymentURL = process.env.DEPLOYMENT_URL ?? "http://127.0.0.1:8787";
+const deploymentURL = process.env.DEPLOYMENT_URL ?? "http://127.0.0.1:3000";
 if (!deploymentURL) {
 	throw new Error("DEPLOYMENT_URL not set");
 }
