0fc80411b514bf8206c5508ea89f2543d568f334
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-10-07 15:16:09 +0300

    revert

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 
-// export default defineConfig({
-//   test: {
-//     typecheck: {
-//       include: ["**/*.test-d.ts"],
-//       exclude: ["examples/**"],
-//       only: true,
-//     },
-//   },
-// });
 export default defineConfig({
   test: {
     typecheck: {
-      include: ['src/**/*.ts', 'src/**/*.tsx', 'test/**/*.ts', 'test/**/*.tsx'],
-      exclude: ['examples/**', '**/node_modules/**'],
-      // ignoreSourceErrors: true,
+      include: ["**/*.test-d.ts"],
+      exclude: ["examples/**"],
       only: true,
     },
   },
