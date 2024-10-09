4629572bc76f37620206998b82bcec00c68a5a04
CahidArda <cahidardaooz@hotmail.com>
2024-10-09 21:45:38 +0300

    fix: send request to route

--- examples/nextjs/ci.test.ts
+++ examples/nextjs/ci.test.ts
@@ -1,13 +1,13 @@
 import { test, expect, } from "bun:test";
 import { DATA, ID } from "./src/app/constants"
 
-const deploymentURL = process.env.DEPLOYMENT_URL ?? "http://127.0.0.1:8787";
+const deploymentURL = process.env.DEPLOYMENT_URL ?? "http://127.0.0.1:3000";
 if (!deploymentURL) {
   throw new Error("DEPLOYMENT_URL not set");
 }
 
 test("the server is running", async () => {
-  const res = await fetch(deploymentURL);
+  const res = await fetch(`${deploymentURL}/api`);  
 
   if (res.status !== 200) {
     console.log(await res.text());
