005738118ef1c5e0a1539f5ccbc648cef3ddb4b5
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-10-10 13:36:40 +0300

    css and label fixes

--- examples/nextjs/src/app/components/upload.tsx
+++ examples/nextjs/src/app/components/upload.tsx
@@ -80,9 +80,9 @@ export default function UploadData() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               className="icon icon-tabler icons-tabler-outline icon-tabler-x text-red-400 group-hover:text-white"
             ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
           </button>
@@ -109,9 +109,9 @@ export default function UploadData() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className="icon icon-tabler icons-tabler-outline icon-tabler-plus  text-blue-400 group-hover:text-white"
           ><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
         </button>
