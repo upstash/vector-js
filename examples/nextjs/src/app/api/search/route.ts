0c0e7fb15c1d03dcf42ed341bace0ebc5cea5a7d
CahidArda <cahidardaooz@hotmail.com>
2024-10-10 13:37:11 +0300

    fix: type error

--- examples/nextjs/src/app/api/search/route.ts
+++ examples/nextjs/src/app/api/search/route.ts
@@ -6,6 +6,6 @@ export const POST = async (request: NextRequest) => {
   const { query } = await request.json() as SearchPayload
 
   const result = await index.query({ data: query, topK: 3, includeData: true })  
-  const response: SearchResponse = { result }
+  const response: SearchResponse = { result } as SearchResponse
   return new NextResponse(JSON.stringify(response), { status: 200 })
 }
