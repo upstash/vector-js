b4fd76dc35aa69068a578305621d03b51bb80d1c
Cahid Arda Öz <cahidardaooz@hotmail.com>
2024-11-06 12:43:12 +0300

    fix: don't sleep after the last retry attempt (#52)

--- src/http/index.test.ts
+++ src/http/index.test.ts
@@ -53,3 +53,32 @@ describe("Abort", () => {
     expect(bodyResponse.result).toEqual("Abort works!");
   });
 });
+
+describe.only("retry", () => {
+  test("should terminate after sleeping 5 times", async () => {
+    // init a cient which will always get errors
+    const client = newHttpClient(
+      {
+        retries: 5,
+        backoff: (retryCount) => Math.exp(retryCount) * 50,
+      },
+      {
+        url: "",
+        token: "non-existent",
+      }
+    );
+
+    // get should take 4.287 seconds and terminate before the timeout.
+    const throws = () =>
+      Promise.race([
+        client.request({
+          path: ["upsert"],
+          body: "wrong-body",
+        }),
+        new Promise((r) => setTimeout(r, 4500)),
+      ]);
+
+    // if the Promise.race doesn't throw, that means the retries took longer than 4.5s
+    expect(throws).toThrow("fetch() URL is invalid");
+  });
+});
